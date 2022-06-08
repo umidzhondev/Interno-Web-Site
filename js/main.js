d6729dc6c3698c96354dd1fa67cc29d0d09827e1
UmidjonDeveloper <umidzhonfullstackdev@gmail.com>
2022-06-08 13:55:11 +0600

    Interno Web Site

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// * Nabvar
+const nav = document.querySelector(".nav");
+const navOpenBtn = document.querySelector(".navbar__open-icon");
+const navCloseBtn = document.querySelector(".navbar__close-icon");
+
+navOpenBtn.addEventListener('click', () => {
+    nav.classList.add("open");
+});
+
+navCloseBtn.addEventListener('click', () => {
+    nav.classList.remove("open");
+});
+
+// * Swiper Js 
+var swiper = new Swiper(".brand--swiper", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    slidesPerView: 5,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        400: {
+          slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        992: {
+          slidesPerView: 5,
+          spaceBetween: 0,
+        },
+      },
+  }); 
